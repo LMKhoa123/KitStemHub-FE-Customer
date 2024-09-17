feb1f15b58f23d2cfa6d32a4dfa0fabfc482a30e
Khoa <khoa1509gg@gmail.com>
2024-09-17 09:44:39 +0700

    add login and signUp code

--- KitStemHub-Customer/src/App.jsx
+++ KitStemHub-Customer/src/App.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import LoginPage from "./pages/loginpage/LoginPage";
 import ProfilePage from "./pages/profilepage/ProfilePage";
+import SignUpPage from "./pages/signuppage/SignUpPage";
 
 function App() {
   const router = createBrowserRouter([
@@ -11,6 +12,10 @@ function App() {
     {
       path: "/profilepage",
       element: <ProfilePage />,
+    },
+    {
+      path: "/signuppage",
+      element: <SignUpPage />,
     },
   ]);
   return <RouterProvider router={router} />;
