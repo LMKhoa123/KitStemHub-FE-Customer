459cfb07d767868917e5dc4e23ab79cd092e2fbf
Khoa <khoa1509gg@gmail.com>
2024-09-19 09:46:08 +0700

    add responsive navbar

--- KitStemHub-Customer/src/components/login/navbar/Navbar.jsx
+++ KitStemHub-Customer/src/components/login/navbar/Navbar.jsx
@@ -1,17 +1,56 @@
-import { Input } from "antd";
+import React, { useState } from "react";
+import { MenuOutlined } from "@ant-design/icons";
+import { Input, Menu, Drawer } from "antd";
 const { Search } = Input;
-const onSearch = (value, _e, info) => console.log(info && info.source, value);
 
+const onSearch = (value) => console.log(value);
+
+const items = [
+  {
+    label: "Home",
+    key: "home",
+  },
+  {
+    label: "Contact Us",
+    key: "contact",
+  },
+  {
+    label: "About Us",
+    key: "about",
+  },
+  {
+    label: "Login",
+    key: "login",
+  },
+  {
+    label: "Sign In",
+    key: "signin",
+  },
+];
 // eslint-disable-next-line react/prop-types
 function Navbar({ childrenItem }) {
+  const [visible, setVisible] = useState(false);
+  const showDrawer = () => {
+    setVisible(true);
+  };
+  const onClose = () => {
+    setVisible(false);
+  };
+
   return (
     <div className="content-wrapper max-w-screen-2xl text-base mx-auto px-8 border-b h-24">
       <header className="py-6 mx-10">
         <nav className="flex flex-row justify-between items-center">
+          {/* Logo */}
           <div className="logo basis-1/6 text-center text-xl font-semibold cursor-pointer">
             KitStemHub
           </div>
-          <ul className=" basis-2/6 flex items-center justify-between text-md font-normal text-gray-400">
+
+          {/* Desktop Menu */}
+          <ul
+            id="kt-menu"
+            className="basis-2/6 hidden lg:flex lg:items-center lg:justify-between text-md font-normal text-gray-400"
+          >
             <li className="kt-menu-item">
               <a href="#" className="">
                 Home
@@ -33,13 +72,32 @@ function Navbar({ childrenItem }) {
               </a>
             </li>
           </ul>
+
+          {/* Desktop Search */}
           <Search
             placeholder="What are you looking for?"
             onSearch={onSearch}
             enterButton
-            className="search-product basis-1/6"
+            className="hidden lg:block lg:basis-2/6"
           />
+
           {childrenItem}
+
+          {/* Mobile Menu Icon */}
+          <div className="lg:hidden basis-1/6 cursor-pointer px-4">
+            <MenuOutlined onClick={showDrawer} />
+          </div>
+
+          {/* Mobile Drawer */}
+          <Drawer
+            title="Menu"
+            placement="right"
+            onClose={onClose}
+            visible={visible}
+            className="lg:hidden"
+          >
+            <Menu items={items} mode="vertical" />
+          </Drawer>
         </nav>
       </header>
     </div>
