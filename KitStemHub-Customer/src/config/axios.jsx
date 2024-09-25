f9117165691c3da108a8c05ba1417b41e22760ac
Khoa <khoa1509gg@gmail.com>
2024-09-25 10:21:47 +0700

    modified token

--- KitStemHub-Customer/src/config/axios.jsx
+++ KitStemHub-Customer/src/config/axios.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-const baseUrl = "http://54.66.193.22:5000/api/";
+const baseUrl = "http://54.66.193.22:5000/api/Users/";
 
 const config = {
   baseUrl: baseUrl,
@@ -17,43 +17,48 @@ const handleBefore = (config) => {
   const token = localStorage.getItem("token")?.replaceAll('"', "");
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
-    // config.headers["Authorization"] = `Bearer ${token}`;
-    // console.log(config.headers.Authorization);
   }
   return config;
 };
 
 api.interceptors.request.use(handleBefore, (error) => Promise.reject(error));
 
-//// Add a response interceptor
+//////////////////////////////////
 api.interceptors.response.use(
-  (response) => {
-    // console.log("API Response:", response.data); // Kiểm tra dữ liệu phản hồi ở đây
-    return response;
-  },
+  (response) => response,
   async (error) => {
     const originalRequest = error.config;
-
+    console.log(originalRequest);
+    console.log("hello" + error.config);
+    console.log("Error Config:", error.config);
+    console.log("Error Response Status:", error.response.status);
+    console.log("Error Message:", error.message);
     // If the error status is 401 and there is no originalRequest._retry flag,
     // it means the token has expired and we need to refresh it
     if (error.response.status === 401 && !originalRequest._retry) {
+      console.log("Token expired or unauthorized - 401 error");
       originalRequest._retry = true;
 
       try {
-        const refreshToken = localStorage.getItem("refreshToken");
-        const response = await axios.post("RefreshToken", { refreshToken });
-        // console.log(response.data);
-        const { accessToken, newRefreshToken } = response.data.details;
+        const refreshToken1 = localStorage.getItem("refreshToken");
+        console.log(refreshToken1);
+        const response = await axios.post(
+          `http://54.66.193.22:5000/api/Users/RefreshToken/${refreshToken1}`
+        );
+        console.log("ggggg" + response.data);
+        const { accessToken, refreshToken } = response.data.details;
         console.log(accessToken);
         localStorage.setItem("token", accessToken);
-        localStorage.setItem("refreshToken", newRefreshToken);
+        localStorage.setItem("refreshToken", refreshToken);
 
         // Retry the original request with the new token
         originalRequest.headers.Authorization = `Bearer ${accessToken}`;
+
         return axios(originalRequest);
       } catch (error) {
         // Xử lý lỗi refresh token (ví dụ: chuyển hướng về trang đăng nhập)
-        window.location.href = "/login";
+        // window.location.href = "/";
+        console.log(error);
         return Promise.reject(error);
       }
     }
