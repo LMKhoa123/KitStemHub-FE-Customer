cd1e5cc65745e77d772f31fde739f79965f7128d
QzUzAzN <mailemisoquan@gmail.com>
2024-10-01 21:58:50 +0700

    update profile 1/10

--- KitStemHub-Customer/src/components/profile/profileaddress/formmyaddress/FormMyAddress.jsx
+++ KitStemHub-Customer/src/components/profile/profileaddress/formmyaddress/FormMyAddress.jsx
@@ -1,5 +1,3 @@
-import {} from "antd";
-
 function FormMyAddress() {
   return (
     <div className=" bg-white p-14 rounded-lg shadow-lg">
@@ -7,14 +5,14 @@ function FormMyAddress() {
         Edit Your Profile
       </h1>
       <div className="form-container flex flex-col md:flex-row gap-6">
-        <form className="flex flex-col gap-6 w-full md:w-1/2">
-          <div className="form-group flex flex-col div1">
-            <div className="w-full px-3 mb-6 ">
+        <form className="flex flex-col w-full md:w-1/2">
+          <div className="form-group flex flex-row mb-3">
+            <div className="w-full px-3 mb-3 ">
               <label
                 className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
                 htmlFor="grid-first-name"
               >
-                Họ và Tên
+                Họ
               </label>
               <input
                 className="appearance-none block w-64 bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
@@ -23,109 +21,58 @@ function FormMyAddress() {
                 placeholder="Jane"
               />
             </div>
-            <div className="w-full mb-6 px-3">
-              <label
-                className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                htmlFor="grid-last-name"
-              >
-                Email
-              </label>
-              <input
-                className="appearance-none block w-64 bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 mb-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                id="grid-last-name"
-                type="email"
-                placeholder="name@example.com"
-              />
-            </div>
-            <div className="w-full mb-6 px-3">
-              <label
-                className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                htmlFor="grid-last-name"
-              >
-                Số điện thoại
-              </label>
-              <input
-                className="appearance-none block w-64 bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 mb-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                id="grid-last-name"
-                type="number"
-                placeholder="name@example.com"
-              />
-            </div>
-          </div>
-        </form>
-
-        <form className="flex flex-col gap-6 w-full md:w-1/2">
-          <div className="form-group flex flex-col div2">
             <div className="w-full px-3 mb-6">
               <label
                 className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
                 htmlFor="grid-first-name"
               >
-                Tỉnh / Thành phố
+                Tên
               </label>
               <input
                 className="appearance-none block w-64 bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
                 id="grid-first-name"
                 type="text"
-                placeholder="Kingston, 5236, United State"
+                placeholder="Jane"
               />
-            </div>
-
-            <div className="w-full px-3 mb-6">
-              <label
-                className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                htmlFor="grid-last-name"
-              >
-                Quận huyện
-              </label>
-              <input
-                className="appearance-none block w-64 bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 mb-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                id="grid-last-name"
-                type="text"
-                placeholder="Kingston, 5236, United State"
-              />
-            </div>
-
-            <div className="w-full px-3 mb-6">
-              <label
-                className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                htmlFor="grid-last-name"
-              >
-                Phường Xã
-              </label>
-              <input
-                className="appearance-none block w-64 bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 mb-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                id="grid-last-name"
-                type="text"
-                placeholder="Kingston, 5236, United State"
-              />
-            </div>
-
-            <div className="w-full px-3 mb-10">
-              <label
-                className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                htmlFor="grid-last-name"
-              >
-                Địa chỉ
-              </label>
-              <input
-                className="appearance-none block w-64 bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 mb-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                id="grid-last-name"
-                type="text"
-                placeholder="Kingston, 5236, United State"
-              />
-            </div>
-
-            <div className="flex justify-end">
-              <div className="mr-3 p-3 font-medium">
-                <a href="#">Cancel</a>
-              </div>
-              <div className="flex bg-red-600 justify-center align-middle p-3 rounded-lg text-white font-medium w-32">
-                <a>Save Changes</a>
-              </div>
             </div>
           </div>
+          <div className="w-full px-3 mb-6 ">
+            <label
+              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              htmlFor="grid-first-name"
+            >
+              Số điện thoại
+            </label>
+            <input
+              className="appearance-none block w-64 bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+              id="grid-first-name"
+              type="text"
+              placeholder=""
+            />
+          </div>
         </form>
+      </div>
+      <div className="w-full mb-6 px-3">
+        <label
+          className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+          htmlFor="grid-last-name"
+        >
+          Thông tin địa chỉ giao hàng
+        </label>
+        <input
+          className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 mb-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+          id="grid-last-name"
+          type="text"
+          placeholder=""
+        />
+      </div>
+      <div className="flex justify-end">
+        <div className="mr-3 p-3 font-medium">
+          <a href="#">Delete All</a>
+        </div>
+        <div className="flex bg-red-600 justify-center align-middle p-3 rounded-lg text-white font-medium w-32">
+          <a>Save Changes</a>
+        </div>
       </div>
     </div>
   );
