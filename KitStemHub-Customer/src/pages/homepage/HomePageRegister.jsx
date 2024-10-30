9271bf537606b36b0f30b7d345e74b6f427dd5ae
Khoa <khoa1509gg@gmail.com>
2024-10-30 10:01:18 +0700

    fix login and show error

--- KitStemHub-Customer/src/pages/homepage/HomePageRegister.jsx
+++ KitStemHub-Customer/src/pages/homepage/HomePageRegister.jsx
@@ -1,8 +1,27 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import Footer from "../../components/footer/Footer";
 import HomeRegister from "../../components/home/HomeRegister";
 import Navbar from "../../components/login/navbar/Navbar";
 
 function HomePageRegister() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const accessToken = localStorage.getItem("token");
+    const refreshToken = localStorage.getItem("refreshToken");
+    console.log(accessToken);
+    if (accessToken && refreshToken) {
+      console.log("Tokens found - Redirecting to home");
+      navigate("/home");
+    } else {
+      console.log("Missing tokens:", {
+        hasAccessToken: !!accessToken,
+        hasRefreshToken: !!refreshToken,
+      });
+    }
+  }, [navigate]);
+
   return (
     <div className="shrink h-screen">
       <Navbar />
